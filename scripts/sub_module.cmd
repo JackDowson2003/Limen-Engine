4d78353c828e70445b77254ef33edf0bf158bf4f
chenlong <2578581718@qq.com>
2026-09-20 18:36:49 +0800

    更新sub_moudule command

--- scripts/sub_module.cmd
+++ scripts/sub_module.cmd
@@ -3,5 +3,7 @@ git submodule add -b docking https://github.com/ocornut/imgui.git LimenEngine/ve
 git submodule add -b docking https://github.com/JackDowson2003/GLFW
 git submodule add https://github.com/g-truc/glm.git LimenEngine/vendor/glm
 git submodule add -b release https://github.com/tinyobjloader/tinyobjloader.git LimenEngine/vendor/tinyobjloader
-
+git submodule add -b v1.x \
+    https://github.com/gabime/spdlog.git \
+    LimenEngine/vendor/spdlog
 git submodule update --init --recursive
